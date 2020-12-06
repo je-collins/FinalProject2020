94c01f4e9fd1b32ace12e10e6b7160b0a3a04848
Jordan Collins <jecollins2019@knights.ucf.edu>
2020-12-06 13:26:37 -0500

    set up

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require('./app/routes/users.routes')(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
---
+++ src/components/superadmin.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import UserDataService from "../services/user.service";
+
+export default class SuperAdmin extends Component {
+
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>hello there super admin!</div>
+        )
+    }
+}
